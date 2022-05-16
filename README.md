5725a8f8fa40ea29cf792a6fe6d67116d05489aa
LNDavid <ldavid@rmacan.com>
2022-05-16 15:23:54 -0400

    add README draft

---
+++ README.md
@@ -0,0 +1,67 @@
+
+![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
+
+# Title
+
+MYSQL CORPORATE TRACKER
+
+## Description
+
+Use Inquirer package to present a series of questions to a user via a CLI.  The answers would subsequently integrate with MYSQL commands that would enable the user to view and change various aspects of an employee's information through a series of 3 interrelated tables.
+
+The following image illustrates the expected output ![expected table display](assets/images/screen-shot-of-expected-output.png).
+
+Output is based on the acceptance criteria outlined in the Module 12 Assignment Overview [Module-12-assignment](https://courses.bootcampspot.com/courses/1181/assignments/23358?module_item_id=464333).
+
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Testing](#testing)
+* [Credits](#credits)
+* [Questions](#questions)
+
+## Installation
+
+To initiate the series of questions, if my coding were correct, typing in node index.js would bring up the first question asking the user to select an option which would subsequently guide the user through the rest of the program.  MYSQL is initiated by typing in "mysql -u root -p" and then entering a mysql password to bring up the mysql prompt.
+
+## Usage
+
+From the three database tables that are created, Department, Role and Employee, a user can view all departments in a database as well as which employees work in specific departments, their names, titles, salaries and managers.  Additional departments and/or roles and/or employees can be added, updated or deleted, with the "SELECT * FROM tablename mysql command allowing the user to see the results of whatever option was selected in the updated table (or even just the specific row - I prefer to show the full table as it provides a broader perspective).
+
+## License
+
+This application is covered under the MIT license.  [MIT license link](https://choosealicense.com/licenses/mit/)
+
+## Contributing
+
+There is still work to be done, as I need to finish the coding to actually pass the responses from the prompts into the commands for the mysql actions. I also need to to fix whatever error is ending inquirer before allowing a user to enter any information.  Any feedback or assistance is always welcome.
+
+## Testing
+
+None at this time.
+
+## Credits
+
+Some assistance was provided in my weekly tutoring session, although we got stuck on the error noted above.  Additional assistance from the TA's during office hours and a number of visits to the Stack Overflow website [https://stackoverflow.com](https://stackoverflow.com).
+
+## Questions?
+
+If you have any questions, please see my contact details below:
+
+## GitHub Username
+
+My GitHub Username is [username](https://github.com/lnd4812).
+
+## GitHub Repository
+
+My GitHub repository link for this project is [github address](https://github.com/lnd4812/mysql-corporate-tracker)
+
+## Application Video Link
+
+The link to the application video may be accessed at to follow
+
+## Contact information
+
+To contact me directly, please feel free to drop me an e-mail at: <a hef="mailto:laureldavid64@gmail.com">laureldavid64@gmail.com</a>
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // const fs = require('fs');
 const inquirer = require("inquirer");
-const cTable = require("console.table");
+const cTable = require('console.table');
 const mysql = require("mysql2");
 const db = require("./db/connection");
 
@@ -36,263 +36,250 @@ function promptTracker() {
       console.log(option);
       tableAction(option);
     })
-}
 
 function tableAction(option) {
     
-      switch(option) {
-
-          case 'view all departments':
-            
-            db.query(`SELECT * FROM department`, (err, result) => {
-                if (err) {
-                  throw(err);
-                }
-                console.table(result);
-            });
-        
-        case 'view all roles':
-
-            db.query(`SELECT * FROM role`, (err, result) => {
-                if (err) {
-                  throw(err);
-                }
-                console.table(result);
-            });
-
-        case 'view all employees':
-
-            db.query(`SELECT * FROM employee`, (err, result) => {
-                if (err) {
-                  throw(err);
-                }
-                console.table(result);
-            });
-
-        case 'add department':
+      if (option === 'view all departments') {
+        db.query(`SELECT * FROM department`, (err, result) => {
+          if (err) {
+            throw(err);
+          }
+          console.table(result);
+        });
+      } else if (option === 'view all roles') {
+        db.query(`SELECT * FROM role`, (err, result) => {
+          if (err) {
+            throw(err);
+          }
+          console.table(result);
+        });
+      } else if (option === 'view all employees') {
+          db.query(`SELECT * FROM employee`, (err, result) => {
+            if (err) {
+              throw(err);
+            }
+            console.table(result);
+          });
+      } else if (option === 'add department') {
+          inquirer
+            .prompt([
+              {
+                type: 'input',
+                name: 'department',
+                message: "Please enter name of department",
+              },
+            ])
+            .then(answers => {
+              db.query(
+                `INSERT INTO department (name)
+                 VALUES (${answers})`,
+                  (err, result) => {
+                    if (err) {
+                      throw(err);
+                    }
+                  console.table(result);
+                  }  
+              );
+            });         
+        } else if (option === 'add a role') {
             inquirer
-                .prompt([
-                    {
-                        type: 'input',
-                        name: 'department',
-                        message: "Please enter name of department",
-                    },
+              .prompt([
+                {
+                  type: 'input',
+                  name: 'title',
+                  message: "Please enter title",
+                },
+                { 
+                  type: 'input',
+                  name: 'salary',
+                  message: "Please enter salary for the role",
+                },
+                {
+                  type: 'input',
+                  name: 'department_id',
+                  message: "Please add department",
+                }
+              ])
+              .then(answers => {
+                db.query(
+                  `INSERT INTO role (title, salary, department_id)
+                   VALUES (${answers})`,
+                      (err, result) => {
+                        if (err) {
+                          throw(err);
+                        }
+                      console.table(result);
+                      }
+                );
+              })
+            
+        } else if (option === 'add employee') {
+            inquirer
+              .prompt([
+                {
+                  type: 'input',
+                  name: 'first_name',
+                  message: "Please enter employee's first name",
+                },
+                {
+                  type: 'input',
+                  name: 'last_name',
+                  message: "Please enter employee's last name",
+                },
+                {
+                  type: 'input',
+                  name: 'role_id',
+                  message: "Please enter employee's title",
+               },
+               {
+                  type: 'input',
+                  name: 'manager_id',
+                  message: "Please enter employee's manager",
+               }
+              ])
+              .then(answers => {
+                db.query(
+                  `INSERT INTO employee (first_name, last_name, role_id, manager_id)
+                   VALUES (${answers})`,
+                    (err, result) => {
+                      if (err) {
+                        throw(err);
+                      }
+                    console.table(result);
+                    }
+                );
+              });
+        } else if (option === 'update employee role') {
+            inquirer
+              .prompt([
+                {
+                  type: 'input',
+                  name: 'employee_id',
+                  message: "Please select employee being updated",
+                },
+                {
+                  type: 'input',
+                  name: 'role_id',
+                  message: "Please select employee's new role",
+                },
+              ])
+              . then(answers => {
+                  db.query(
+                    `UPDATE employee
+                     SET role_id = (${answers[1]}) WHERE id = (${answers[0]})`,
+                       (err, result) => {
+                          if (err) {
+                            throw(err);
+                          }
+                        console.table(result);
+                        }
+                  );
+                });
+        } else if (option === 'update employee manager') {
+            inquirer
+             .prompt([
+               {
+                  type: 'input',
+                  name: 'employee_id',
+                  message: "Please select employee whose manager is changing",
+                },
+                {
+                  type: 'input',
+                  name: 'managername',
+                  message: "Please select employee's new manager",
+                },
+              ])
+              . then(answers => {
+                  db.query(
+                    `UPDATE employee
+                     SET manager_id = (${answers[1]}) WHERE id = (${answers[0]})`,
+                       (err, result) => {
+                          if (err) {
+                            throw(err);
+                          }
+                        console.table(result);
+                      }
+                  );
+              }
+            );
+
+        } else if (option === 'view employees by manager') {
+            inquirer
+              .prompt([
+                {
+                  type: 'input',
+                  name: 'managername',
+                  message: "Please select manager",
+                },
                 ])
-                  .then(answers => {
+                . then(answers => {
                     db.query(
-                        `INSERT INTO department (name)
-                              VALUES ${answers}`,
+                      `SELECT employee.*, role.name AS role_name
+                      FROM employee
+                      LEFT JOIN role ON employee.role.name_id = ${answers}_id`,
                         (err, result) => {
                           if (err) {
                             throw(err);
                           }
-                          console.table(result);
-                          }  
+                        console.table(result);
+                      }
                     );
-                  });         
-
-          case 'add a role':
-              inquirer
-                  .prompt([
-                      {
-                          type: 'input',
-                          name: 'title',
-                          message: "Please enter title",
-                      },
-                      { 
-                          type: 'input',
-                          name: 'salary',
-                          message: "Please enter salary for the role",
-                      },
-                      {
-                          type: 'input',
-                          name: 'department_id',
-                          message: "Please add department",
-                      }
-                  ])
-                  .then(answers => {
-                    db.query(
-                        `INSERT INTO role (title, salary, department_id)
-                              VALUES ${answers}`,
-                        (err, row) => {
-                          if (err) {
-                            throw(err);
-                          }
-                          console.table(row);
-                        }
-                      );
-                  });
-
-          case 'add employee':
-              inquirer
-                  .prompt([
-                      {
-                          type: 'input',
-                          name: 'first_name',
-                          message: "Please enter employee's first name",
-                      },
-                      {
-                          type: 'input',
-                          name: 'last_name',
-                          message: "Please enter employee's last name",
-                      },
-                      {
-                          type: 'input',
-                          name: 'role_id',
-                          message: "Please enter employee's title",
-                      },
-                      {
-                          type: 'input',
-                          name: 'manager_id',
-                          message: "Please enter employee's manager",
-                      }
-                  ])
-                  .then(answers => {
-                    db.query(
-                        `INSERT INTO employee (first_name, last_name, role_id, manager_id)
-                              VALUES ${answers}`,
+                });
+        } else if (option === 'view employees by department') {
+            inquirer
+              .prompt([
+                {
+                  type: 'input',
+                  name: 'department_id',
+                  message: "Please select department",
+                },
+              ])
+              . then(answers => {
+                  db.query(
+                    `SELECT employee.*, role.department_id AS department_id
+                    FROM employee
+                    LEFT JOIN role.department ON employee.role._id = role.${answers}.id 
+                      ${answers}`,
                         (err, result) => {
                           if (err) {
                             throw(err);
                           }
-                          console.table(result);
+                        console.table(result);
                         }
-                      );
-                  });
-
-          case 'update employee role':
-              inquirer
-                  .prompt([
-                      {
-                          type: 'input',
-                          name: 'employee_id',
-                          message: "Please select employee being updated",
-                      },
-                      {
-                          type: 'input',
-                          name: 'role_id',
-                          message: "Please select employee's new role",
-                      },
-                  ])
-                  . then(answers => {
-                    db.query(
-                        `UPDATE employee
-                              SET role_id = ${answers} WHERE id = employee_id`,
+                  );
+              });
+        } else if (option === 'delete department') {
+            inquirer
+             .prompt([
+                {
+                  type: 'input',
+                  name: 'department_id',
+                  message: "Which department do you want to delete?",
+                },
+              ])
+              . then(answers => {
+                  db.query(
+                    `DELETE FROM department WHERE id = ${answers}`,
                         (err, result) => {
                           if (err) {
-                            throw(err);
-                          }
-                          console.table(result);
+                            throw(err);;
                         }
-                      );
-                  });
-
-          case 'update employee manager':
-              inquirer
-                  .prompt([
-                      {
-                          type: 'input',
-                          name: 'employee_id',
-                          message: "Please select employee whose manager is changing",
-                      },
-                      {
-                          type: 'input',
-                          name: 'managername',
-                          message: "Please select employee's new manager",
-                      },
-                  ])
-                  . then(answers => {
-                    db.query(
-                        `UPDATE employee
-                              SET manager_id = ${answers} WHERE id = employee_id`,
-                        (err, result) => {
-                          if (err) {
-                            throw(err);
-                          }
-                          console.table(result);
-                        }
-                      );
-                  });
-
-          case 'view employees by manager':
-              inquirer
-                  .prompt([
-                      {
-                          type: 'input',
-                          name: 'managername',
-                          message: "Please select manager",
-                      },
-                  ])
-                  . then(answers => {
-                    db.query(
-                        `SELECT employee.*, role.name AS role_name
-                        FROM employee
-                        LEFT JOIN role ON employee.role.name_id = ${answers}_id`,
-                        (err, result) => {
-                          if (err) {
-                            throw(err);
-                          }
-                          console.table(result);
-                        }
-                      );
-                  });
-
-          case 'view employees by department':
-              inquirer
-                  .prompt([
-                      {
-                          type: 'input',
-                          name: 'department_id',
-                          message: "Please select department",
-                      },
-                  ])
-                  . then(answers => {
-                    db.query(
-                        `SELECT employee.*, role.department_id AS department_id
-                        FROM employee
-                        LEFT JOIN role.department ON employee.role._id = role.${answers}.id 
-                         ${answers}`,
-                        (err, result) => {
-                          if (err) {
-                            throw(err);
-                          }
-                          console.table(result);
-                        }
-                      );
-                  });
-
-          case 'delete department':
-              inquirer
-                  .prompt([
-                      {
-                          type: 'input',
-                          name: 'department_id',
-                          message: "Which department do you want to delete?",
-                      },
-                  ])
-                  . then(answers => {
-                    db.query(
-                        `DELETE FROM department WHERE id = ${answers}`,
-                        (err, result) => {
-                          if (err) {
-                            console.log(err);
-                          }
                           console.table(result);
                         }
                     )
-                }); // does this have to work around FK Constraints?
-          case "view total utilized budget for department":
+                }); 
+          } else if (option === "view total utilized budget for department") {
               inquirer
-                  .prompt([
-                      {
-                          type: 'input',
-                          name: 'department_id',
-                          message: "Which department's salary do you want to view?",
-                      },
-                  ])
-                  . then(answers => {
+                .prompt([
+                  {
+                    type: 'input',
+                    name: 'department_id',
+                    message: "Which department's salary do you want to view?",
+                  },
+                ])
+                . then(answers => {
                     db.query(
-                        `SELECT SUM(salary) FROM role WHERE role.department_id = ${answers}`,
+                      `SELECT SUM(salary) FROM role WHERE role.department_id = ${answers}`,
                         (err, result) => {
                           if (err) {
                             throw(err);
@@ -300,12 +287,12 @@ function tableAction(option) {
                           console.table(result);
                         }
                     );
-
-        })
-    }
+                });
+              }
+            }  
 }
 async function init() {
-    await promptTracker();
+  await promptTracker();
 };
-    
+
 init();
